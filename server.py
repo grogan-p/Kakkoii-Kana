022e620596c63daa91a8d98e1d8dbd568ae91f88
Patrick Grogan <grogan.p@northeastern.edu>
2026-05-04 22:22:51 -0400

    initial commit

---
+++ server.py
@@ -0,0 +1,39 @@
+import os 
+from flask import Flask, render_template, request, session, jsonify, Response
+from waitress import serve
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
+def index() -> str | Response:
+    """Main function"""
+    questions = {"すし" : "sushi", "はい" : "hai", "にほん" : "nihon"}
+    keys = list(questions.keys())
+
+    if "current_index" not in session:
+        session["current_index"] = 0
+
+    current_index = session["current_index"]
+    kana = keys[current_index]
+    answer = questions[kana]
+
+    if request.method == "POST":
+        response = request.form.get("response")
+        if response:
+            if response.lower() == answer:
+                print(f"Correct! -- question #{current_index + 1}")
+            else:
+                print(f"Incorrect -- question #{current_index + 1}")
+            session["current_index"] = (current_index + 1) % len(questions)
+        new_kana = keys[session["current_index"]]
+        return jsonify(word=new_kana)
+
+    return render_template('index.html', word=kana, current_index=session["current_index"])
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=7000)
